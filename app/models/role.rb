33b4ed5e83e375dbef91bc467c448003678fe598
lister <simon@lisuna.org>
2010-02-12 16:15:41 +0100

    added database associations

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,5 +1,7 @@
 class Role < ActiveRecord::Base
-  has_and_belongs_to_many :users
+ # has_and_belongs_to_many :users
+  has_many :roleuser_associations
+  has_many :users, :through => :roleuser_associations
 
-#	using_access_control
+  using_access_control
 end
