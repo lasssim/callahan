befcff3e7cc52d5b47920663378e4b6e733eb679
lister <simon@lisuna.org>
2010-02-12 16:20:05 +0100

    changed rails version

--- config/environment.rb
+++ config/environment.rb
@@ -5,7 +5,7 @@
 # ENV['RAILS_ENV'] ||= 'production'
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.1.2' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.4' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -67,4 +67,11 @@ Rails::Initializer.run do |config|
 
   # Activate observers that should always be running
   # config.active_record.observers = :cacher, :garbage_collector
+
+  #ActiveSupport::CoreExtensions::Date::Conversions::DATE_FORMATS.merge!(
+  #  :birthday    => "%A, %B %d, %Y",
+  #  :plays_since => "%B %Y"
+  #)
+
+
 end
