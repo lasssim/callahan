506ebf3f2c6408b1873bf9d8e72a4dd57a049b71
lister <simon@lisuna.org>
2010-02-12 16:18:46 +0100

    reworked authorization

--- config/authorization_rules.rb
+++ config/authorization_rules.rb
@@ -1,19 +1,31 @@
 authorization do  
   role :user do
-    has_permission_on :roles, :to => [:index, :show]
-    has_permission_on :users, :to => [:index, :show]
-    has_permission_on :users, :to => [:edit, :update] do  
+    has_permission_on :roles,         :to => :read
+    has_permission_on :user_sessions, :to => [:delete, :update]
+
+    has_permission_on :users, :to => :read
+    has_permission_on :users, :to => :update do  
       if_attribute :id => is { user.id }  
-    end 
+    end
   end
 
   role :admin do  
-    has_permission_on [:users, :roles], :to => [:index, :show, :new, :create, :edit, :update, :destroy]  
+    has_permission_on :roleuser_associations, :to => :manage
+    has_permission_on :users,                 :to => :manage
+    has_permission_on :roles,                 :to => :manage 
+    has_permission_on :authorization_rules,   :to => :read
   end  
 
   role :guest do  
-    has_permission_on :users,         :to => [:new, :index, :create]  
-    has_permission_on :user_sessions, :to => [:new, :index, :create]
+    has_permission_on :users,                 :to => :create
+    has_permission_on :roleuser_associations, :to => :create
   end  
 end
 
+privileges do
+  privilege :manage, :includes => [:create, :read, :update, :delete]
+  privilege :read,   :includes => [:index, :show]
+  privilege :create, :includes => :new
+  privilege :update, :includes => :edit
+  privilege :delete, :includes => :destroy
+end
