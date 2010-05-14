acfcd8e5805c58f6e4c41707674b996fd872f997
simon <simon@tonks.(none)>
2010-05-14 16:04:56 +0200

    added ruby_parser gem; switched to rails 2.3.5; added some setting to prevent a bug (unsuccessful yet)

--- config/environment.rb
+++ config/environment.rb
@@ -5,7 +5,7 @@
 # ENV['RAILS_ENV'] ||= 'production'
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.4' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -26,6 +26,7 @@ Rails::Initializer.run do |config|
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "aws-s3", :lib => "aws/s3"
   config.gem "authlogic"
+  config.gem "ruby_parser"
 #  config.gem "hpricot"
 
   # Only load the plugins named here, in the order given. By default, all plugins 
@@ -45,7 +46,9 @@ Rails::Initializer.run do |config|
   # Make Time.zone default to the specified zone, and make Active Record store time values
   # in the database in UTC, and return them converted to the specified local zone.
   # Run "rake -D time" for a list of tasks for finding time zone names. Comment line to use default local time.
-  config.time_zone = 'UTC'
+#config.time_zone = 'UTC'
+  config.active_record.default_timezone = :utc
+  config.cache_classes = true
 
   # Your secret key for verifying cookie session data integrity.
   # If you change this key, all old sessions will become invalid!
