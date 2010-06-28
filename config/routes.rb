86ae5f4b75e4bbe8620754b329e83ba226a7a612
simon <simon@tonks.(none)>
2010-06-28 20:56:10 +0200

    added some routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,24 @@
 ActionController::Routing::Routes.draw do |map|
-  map.resources :teams
+  map.calendar '/calendar/:year/:month', :controller => 'calendar', :action => 'index', :year => Time.now.year, :month => Time.now.month
 
+  map.resources :clubs
+  map.resources :teams
   map.resources :associations
+  map.resources :inquiries
 
   map.resources :users
+  map.resources :users do |users|
+    users.resources :messages, :collection => { :delete_selected => :post }
+  end
+
+
   map.resources :roles
   map.resources :password_resets
   map.resources :editor
   map.resources :tournament_reports
   
   map.resource :user_session
+ 
   
 
   #map.resource :account, :controller => "users"
