b41dee69503beafe3515346689c3c7c5ead89492
simon <simon@tonks.(none)>
2010-06-28 20:52:38 +0200

    added firends

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,6 +17,41 @@ class User < ActiveRecord::Base
                                         },
                              :processors => [:thumbnail, :effect]
   
+  has_private_messages
+
+  acts_as_inquirable   :actions => [:friendship]
+  acts_as_inquiry_item :actions => [:friendship]
+
+
+
+  ###########################
+  # Friends
+  ###########################
+  has_many :forward_friendships, :class_name => "Friendship", :foreign_key => "user_id"
+  has_many :inverse_friendships, :class_name => "Friendship", :foreign_key => "friend_id"  
+  
+  has_many :forward_friends, :through => :forward_friendships, :source => :friend
+  has_many :inverse_friends, :through => :inverse_friendships, :source => :user 
+
+  attr_reader :friends
+
+  def friends
+    (forward_friends + inverse_friends).uniq
+  end
+
+  def is_my_friend?(_user)
+    friends.include?(_user)
+  end
+
+  def accept_friend(_inquiry)
+    friends << _inquiry.inquirer
+  end
+
+  def decline_friend(_inquiry)
+
+  end
+
+
   def deliver_password_reset_instructions!
     reset_perishable_token!
     Notifier.deliver_password_reset_instructions(self)
