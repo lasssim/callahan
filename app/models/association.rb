4335fe50910e79b3416eaca0064d9f442e61159c
simon <simon@tonks.(none)>
2010-06-28 20:52:04 +0200

    first working version of assocs

--- app/models/association.rb
+++ app/models/association.rb
@@ -1,7 +1,9 @@
 class Association < ActiveRecord::Base
   belongs_to :owner, :class_name => "User", :foreign_key => "owner_id"
   has_many   :teams
-  
+  has_many   :invitations, :as => :inviteable
+
+
   #has_and_belongs_to_many :users
   
   acts_as_nested_set
@@ -9,6 +11,8 @@ class Association < ActiveRecord::Base
   acts_as_mappable 
   before_validation_on_create :geocode_realm
   validate :validate_geocode
+
+  acts_as_inquiry_item :actions => [:ownership]
 
   attr_reader :coords
 
@@ -19,16 +23,49 @@ class Association < ActiveRecord::Base
 
   private
   def geocode_realm
+    return if realm == "International"
     geo=Geokit::Geocoders::MultiGeocoder.geocode(realm)
     #errors.add(:realm, "Could not Geocode address") if !geo.success
-    self.lat, self.lng = geo.lat,geo.lng if geo.success
+    if geo.success
+      self.lat, self.lng = geo.lat,geo.lng 
+#      set_country_code
+    end
   end
  
   def validate_geocode
+    return if realm == "International"
     geo=Geokit::Geocoders::MultiGeocoder.geocode(realm)
-    errors.add(:realm, "Could not Geocode realm.") if !geo.success
-    self.lat, self.lng = geo.lat,geo.lng if geo.success
+    if geo.success
+      self.lat, self.lng = geo.lat,geo.lng 
+#     set_country_code
+    else
+#      errors.add(:realm, "Could not Geocode realm.")
+    end
   end
- 
+
+  def accept_ownership(_inquiry)
+    self.owner = _inquiry.inquirer
+    save
+  end
+
 
 end
+#  def set_country_code
+#    cc = Geokit::Geocoders::GoogleGeocoder.do_reverse_geocode(self.coords).country_code
+#    if cc
+#      self.country_code = cc.downcase
+#    else
+#      self.country_code = ""
+#    end
+#  end
+#
+#end
+#
+#class RootAssociation < Association
+#  attr_reader :realm
+#  private
+#    def realm
+#      "International"
+#    end
+#end
+
