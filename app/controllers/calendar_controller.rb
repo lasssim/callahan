747e17d62724ac53b8d963ff0190d73c13b58af2
Sorien <Rene.Swoboda@gmail.com>
2010-07-05 19:21:05 +0200

    fixed calendar routing

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -1,9 +1,11 @@
 class CalendarController < ApplicationController
   
+  navigation :calendar_root
+  
   def index
-    @month = params[:month].to_i
-    @year = params[:year].to_i
-
+    @month = (params[:month] || Time.zone.now.month).to_i
+    @year = (params[:year] || Time.zone.now.year).to_i
+    
     @shown_month = Date.civil(@year, @month)
 
     @event_strips = Event.event_strips_for_month(@shown_month)
